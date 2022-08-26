e274a6c6006595920737c1b55fb04218c53a9dac
samanwoysaha09 <samanwoysaha09@gmail.com>
2022-08-26 14:14:06 +0530

    first commit

---
+++ pd.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+
+df1 = pd.DataFrame()
+new1 = pd.DataFrame()
+uploaded_files = st.file_uploader("Choose a file", type=["xlsx"], accept_multiple_files=True)
+if uploaded_files is not None:
+    for uploaded_file in uploaded_files:
+        df1 = pd.read_excel(uploaded_file, sheet_name= "Sheet1")
+        new1 = new1.append(df1, ignore_index = True)
+        
+    st.table(new1)
+mergebtn = st.button("merge")
+if mergebtn :
+    new1.to_excel("new_file.xlsx", sheet_name="Sheet1", index=False)
+    st.success("Merged successfully")
+
+
