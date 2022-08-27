d60ee1b81807b0e777f7125f88eb5d478dddad99
samanwoysaha09 <samanwoysaha09@gmail.com>
2022-08-27 20:07:03 +0530

    small update

--- pd.py
+++ pd.py
@@ -18,10 +18,10 @@ if uploaded_files is not None:
         new1 = new1.append(df1, ignore_index = True)
         df2 = pd.read_excel(uploaded_file, sheet_name= "Sheet2")
         new2 = new2.append(df2, ignore_index = True)
-        # df3 = pd.read_excel(uploaded_file, sheet_name= "Sheet3")
-        # new3 = new3.append(df3, ignore_index = True)
-        # df4 = pd.read_excel(uploaded_file, sheet_name= "Sheet4")
-        # new4 = new4.append(df4, ignore_index = True)
+        df3 = pd.read_excel(uploaded_file, sheet_name= "Sheet3")
+        new3 = new3.append(df3, ignore_index = True)
+        df4 = pd.read_excel(uploaded_file, sheet_name= "Sheet4")
+        new4 = new4.append(df4, ignore_index = True)
         
 
 mergebtn = st.button("merge")
@@ -30,8 +30,8 @@ if mergebtn :
     writer = pd.ExcelWriter('new_file.xlsx')
     new1.to_excel(writer, sheet_name="Sheet1", index=False)
     new2.to_excel(writer, sheet_name="Sheet2", index=False)
-    # new3.to_excel(writer, sheet_name="Sheet3", index=False)
-    # new4.to_excel(writer, sheet_name="Sheet4", index=False)
+    new3.to_excel(writer, sheet_name="Sheet3", index=False)
+    new4.to_excel(writer, sheet_name="Sheet4", index=False)
     writer.save()
     writer.close()
 
