2b3485d989fedca20de326c323e433a5c75b7a7d
samanwoysaha09 <samanwoysaha09@gmail.com>
2022-08-27 21:19:13 +0530

    readme,sh,reqquirements added

---
+++ README.md
@@ -0,0 +1,18 @@
+# Merge Excel Sheets
+
+## Install and run by shell file
+- sh run.sh
+
+# OR
+
+## Install and run by requirements.txt
+
+- pip3 install -r requirements.txt
+- streamlit run pd.py
+
+## Usage 
+
+- Open the given URL link
+- Upload multiple excel files
+- Note: "Every excel file has to be same number of excel sheets"
+- Allowed sheet names : "RAW DATA" , "APP ALERTS", "PIVOT", "COUNT" .
--- pd.py
+++ pd.py
@@ -14,13 +14,13 @@ uploaded_files = st.file_uploader("Choose a file", type=["xlsx"], accept_multipl
 if uploaded_files is not None:
 
     for uploaded_file in uploaded_files:
-        df1 = pd.read_excel(uploaded_file, sheet_name= "Sheet1")
+        df1 = pd.read_excel(uploaded_file, sheet_name= "RAW DATA")
         new1 = new1.append(df1, ignore_index = True)
-        df2 = pd.read_excel(uploaded_file, sheet_name= "Sheet2")
+        df2 = pd.read_excel(uploaded_file, sheet_name= "APP ALERTS")
         new2 = new2.append(df2, ignore_index = True)
-        df3 = pd.read_excel(uploaded_file, sheet_name= "Sheet3")
+        df3 = pd.read_excel(uploaded_file, sheet_name= "PIVOT")
         new3 = new3.append(df3, ignore_index = True)
-        df4 = pd.read_excel(uploaded_file, sheet_name= "Sheet4")
+        df4 = pd.read_excel(uploaded_file, sheet_name= "COUNT")
         new4 = new4.append(df4, ignore_index = True)
         
 
@@ -28,10 +28,10 @@ mergebtn = st.button("merge")
 if mergebtn :
     
     writer = pd.ExcelWriter('new_file.xlsx')
-    new1.to_excel(writer, sheet_name="Sheet1", index=False)
-    new2.to_excel(writer, sheet_name="Sheet2", index=False)
-    new3.to_excel(writer, sheet_name="Sheet3", index=False)
-    new4.to_excel(writer, sheet_name="Sheet4", index=False)
+    new1.to_excel(writer, sheet_name="RAW DATA", index=False)
+    new2.to_excel(writer, sheet_name="APP ALERTS", index=False)
+    new3.to_excel(writer, sheet_name="PIVOT", index=False)
+    new4.to_excel(writer, sheet_name="COUNT", index=False)
     writer.save()
     writer.close()
 
